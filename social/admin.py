4ceec3db4d803613d603b880cf6b99cdd25231bd
Daniel Floresca <dfloresca@gmail.com>
2024-02-17 15:26:24 -0800

    feat: updated admin.py views, created Profile model as well

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+# Unregister Groups
+admin.site.unregister(Group)
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username fields on admin page
+    fields = ["username"]
+
+# Unregister User
+admin.site.unregister(User)
+
+#  Register new User Admin with customized fields
+admin.site.register(User, UserAdmin)
--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create a User Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # One to one relationship with the User model
+    follows = models.ManyToManyField(
+        'self', 
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True
+        )
