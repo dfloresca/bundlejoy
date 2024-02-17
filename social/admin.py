b7b6f603b31cc1f3d8dcc236fb6a8a0a4bbf30f0
Daniel Floresca <dfloresca@gmail.com>
2024-02-17 15:31:30 -0800

    feat: added __srt__ to profile class and ProfileInline

--- social/admin.py
+++ social/admin.py
@@ -1,17 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import Profile
 
 # Unregister Groups
 admin.site.unregister(Group)
+
+# Mix profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
 
 # Extend User Model
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Just display username fields on admin page
     fields = ["username"]
+    inlines = [ProfileInline]
 
 # Unregister User
 admin.site.unregister(User)
 
 #  Register new User Admin with customized fields
 admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
