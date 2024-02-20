c55ed5eb8e51ec1b4a53bfd4fdce6e0046e6afdd
Daniel Floresca <dfloresca@gmail.com>
2024-02-19 19:36:31 -0800

    Feat: added timezone to settings.py, created profile_list view in views.py

--- social/views.py
+++ social/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Profile
+
+
 
 # Create your views here. 
 def home(request):
     return render(request, 'home.html', {})
 
 def profile_list(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'profile_list.html', { 'profiles': profiles})
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', { 'profiles': profiles})
+    else:
+        messages.success(request, ("You must be logged in to view this page"))
+        return redirect('home')
