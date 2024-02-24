22267e5cc8087652d06efc7394855581587b709f
Daniel Floresca <dfloresca@gmail.com>
2024-02-23 17:40:35 -0800

    feat: updated models.py to add social links and profile_bio, created and ran migrations

--- social/models.py
+++ social/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
     
-    
+
     def __str__(self):
         return(
             f"{self.user}"
@@ -36,6 +36,11 @@ class Profile(models.Model):
     
     date_modified = models.DateTimeField(User, auto_now=True)
     profile_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    
+    profile_bio = models.CharField(null=True, blank=True, max_length=500)
+    homepage_link = models.CharField(null=True, blank=True, max_length=50)
+    facebook_link = models.CharField(null=True, blank=True, max_length=50)
+    instagram_link = models.CharField(null=True, blank=True, max_length=50)
         
     def __str__(self):
         return self.user.username
