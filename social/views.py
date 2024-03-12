b65d60105cca4ae7bb7caec682795aa42453243a
Daniel Floresca <dfloresca@gmail.com>
2024-03-11 21:19:34 -0700

    feat: added @permission required decorator to profile

--- social/views.py
+++ social/views.py
@@ -38,33 +38,30 @@ def home(request):
         return render(request, 'home.html', {"posts": posts, "form": form, "show_comment": show_comment})
     
 
-@permission_required("home", login_url="/login/")
+@permission_required("profile_list", login_url="/login/")
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, 'profile_list.html', { 'profiles': profiles})
     
-    
+@permission_required("profile", login_url="/login/")    
 def profile(request, pk):
-    if request.user.is_authenticated:
-        profile = Profile.objects.get(user_id=pk)
-        posts = Post.objects.filter(user_id=pk).order_by("-created_at")
-        #post form logic
-        if request.method=="POST":
-            # Get current user 
-            current_user_profile = request.user.profile
-            # Get Form Data
-            action = request.POST['follow']
-            # Decide to follow or unfollow
-            if action == "unfollow":
-                current_user_profile.follows.remove(profile)
-            elif action == "follow":
-                current_user_profile.follows.add(profile)
-                # Save the profile
-            current_user_profile.save()
-        return render(request, "profile.html", {"profile":profile, "posts":posts })
-    else:
-        messages.success(request, ("You must be logged in to view this page"))
-        return redirect('home')
+    profile = Profile.objects.get(user_id=pk)
+    posts = Post.objects.filter(user_id=pk).order_by("-created_at")
+    #post form logic
+    if request.method=="POST":
+        # Get current user 
+        current_user_profile = request.user.profile
+        # Get Form Data
+        action = request.POST['follow']
+        # Decide to follow or unfollow
+        if action == "unfollow":
+            current_user_profile.follows.remove(profile)
+        elif action == "follow":
+            current_user_profile.follows.add(profile)
+            # Save the profile
+        current_user_profile.save()
+    return render(request, "profile.html", {"profile":profile, "posts":posts })
+    
     
 def login_user(request):
     if request.method == 'POST':
